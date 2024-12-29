c98c7a6ea45927827535a55a93903bcbb904749d
maski <maskikarsrikar@gmail.com>
2024-12-29 20:35:46 +0530

    Build 2.1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { motion } from "framer-motion"
-import { Github, Linkedin, Mail, Terminal, Menu } from 'lucide-react'
+import { Github, Linkedin, Mail, Terminal } from 'lucide-react'
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
