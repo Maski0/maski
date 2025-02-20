f65fb57b3370ad54d9c02041fdd315ccbb9071a7
maski <maskikarsrikar@gmail.com>
2025-02-20 22:34:31 +0530

    config changes.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/Maski", // Replace with your actual repo name
+  assetPrefix: "/Maski/"
 };
 
 export default nextConfig;
